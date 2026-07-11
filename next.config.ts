c2225cc5d96f0bb671aa52c815bbcbd7c5493138
Akshat9999572 <akshatshukla2019@gmail.com>
2026-07-11 21:06:03 +0530

    Allow GitHub-hosted image assets

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        pathname: "/Akshat9999572/Gajraj-Legion/main/public/assets/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
